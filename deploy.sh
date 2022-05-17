4d3c1a94b77bca77622395d3edb6f4c0548474f0
开源海哥 <fuhai999@gmail.com>
2022-05-17 10:13:52 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+#npm run build
+
+ossutil rm oss://bsformbuilder/ -rf
+ossutil cp -rf ./index.html  oss://bsformbuilder/index.html
+ossutil cp -rf ./build  oss://bsformbuilder/build
+ossutil cp -rf ./dist  oss://bsformbuilder/dist
+ossutil cp -rf ./src  oss://bsformbuilder/src

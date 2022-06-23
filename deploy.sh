5206ca203299fc83345cab33e0748b14cf6f6318
开源海哥 <fuhai999@gmail.com>
2022-06-23 18:40:19 +0800

    update custom.html

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ set -e
 
 ossutil rm oss://bsformbuilder/ -rf
 ossutil cp -rf ./index.html  oss://bsformbuilder/index.html
+ossutil cp -rf ./custom.html  oss://bsformbuilder/custom.html
 ossutil cp -rf ./build  oss://bsformbuilder/build
 ossutil cp -rf ./dist  oss://bsformbuilder/dist
 ossutil cp -rf ./src  oss://bsformbuilder/src

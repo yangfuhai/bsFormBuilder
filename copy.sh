16bbf8cfcd7a78d2b46810ab61b4bd1a6f8027d7
开源海哥 <fuhai999@gmail.com>
2022-08-28 12:17:20 +0800

    v1.0.8 released!

--- copy.sh
+++ copy.sh
@@ -8,3 +8,6 @@ npm run build
 
 cp ./src/* /Users/michael/work/git/jpress5/jpress-web/src/main/webapp/static/components/bs-form-builder
 cp ./dist/* /Users/michael/work/git/jpress5/jpress-web/src/main/webapp/static/components/bs-form-builder
+
+cp ./src/* /Users/michael/work/git/JPressPro/jpress-web/jpress-web-admin/src/main/webapp/static/components/bs-form-builder
+cp ./dist/* /Users/michael/work/git/JPressPro/jpress-web/jpress-web-admin/src/main/webapp/static/components/bs-form-builder
